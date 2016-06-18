8d3e23aeed4261249c813291fa5a84df03a751d1
Eric Leung <eric@erictleung.com>
2016-06-17 20:31:43 -0700

    Add documentation to splitdf.R

--- bin/splitdf.R
+++ bin/splitdf.R
@@ -1,5 +1,13 @@
-# Source:
-# http://www.gettinggeneticsdone.com/2011/02/split-data-frame-into-testing-and.html
+#' Split Data Frame to Training and Test Set
+#'
+#' @param dataframe
+#' @param trainSplit
+#' @param seed
+#'
+#' @return
+#' @references http://www.gettinggeneticsdone.com/2011/02/split-data-frame-into-testing-and.html
+#'
+#' @examples
 splitdf <- function(dataframe, trainSplit, seed=NULL) {
     if (!is.null(seed)) set.seed(seed)
     index <- 1:nrow(dataframe)
